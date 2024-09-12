da24f9de9e2b8dd268abbfbcc062d6407666ab59
Joe Lee <joe.p.lee@outlook.com>
2024-09-12 08:04:15 -0500

    added /signin endpoint

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const knex = require('knex');
+const bcrypt = require('bcryptjs');
 
 const db = knex({
   client: 'pg',
@@ -44,27 +45,51 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', (req, res) => {
-	if (req.body.email === database.users[0].email &&
-		req.body.password === database.users[0].password) {
-		res.json(database.users[0]);
-	} else {
-		res.status(400).json('error logging in');
-	}
+	db.select('email', 'hash').from('login')
+		.where('email', '=', req.body.email)
+		.then(data => {
+			const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+			if (isValid) {
+				return db.select('*').from('users')
+					.where('email', '=', req.body.email)
+					.then(user => {
+						res.json(user[0])
+					})
+					.catch(err => res.status(400).json('unable to get user'))
+			} else {
+				res.status(400).json('wrong credentials')
+			}
+		})
+		.catch(err => res.status(400).json('wrong credentials'))
 })
 
 app.post('/register', (req, res) => {
 	const { email, name, password } = req.body;
-	db('users')
-		.returning('*')
-		.insert({
-			email: email,
-			name: name,
-			joined: new Date()
+	var salt = bcrypt.genSaltSync(10);
+	var hash = bcrypt.hashSync(password, salt);
+	db.transaction(trx => {
+		trx.insert({
+			hash: hash,
+			email: email
 		})
-		.then(user => {
-			res.json(user[0]);
+		.into('login')
+		.returning('email')
+		.then(loginEmail => {
+			return trx('users')
+				.returning('*')
+				.insert({
+					email: loginEmail[0].email,
+					name: name,
+					joined: new Date()
+				})
+				.then(user => {
+					res.json(user[0]);
+				})
 		})
-		.catch(err => res.status(400).json('unable to register'))
+		.then(trx.commit)
+		.catch(trx.rollback)
+ 	})
+	.catch(err => res.status(400).json('unable to register'))
 })
 
 app.get('/profile/:id', (req, res) => {
@@ -87,8 +112,9 @@ app.put('/image', (req, res) => {
 		.increment('entries', 1)
 		.returning('entries')
 		.then(entries => {
-			console.log(entries);
+			console.log(entries[0].entries);
 		})
+		.catch(err => res.status(400).json('Unable to get entries'))
 })
 
 app.listen(3000, ()=> {
