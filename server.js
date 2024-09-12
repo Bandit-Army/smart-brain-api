f5bc0111752acd01b4959e81ffd637a8145507df
Joe Lee <joe.p.lee@outlook.com>
2024-09-12 09:25:16 -0500

    fixed issue with /image endpoint

--- server.js
+++ server.js
@@ -19,29 +19,29 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const database = {
-	users: [
-		{
-			id: '123',
-			name: 'John',
-			email: 'john@gmail.com',
-			password: 'cookies',
-			entries: 0,
-			joined: new Date()
-		},
-		{
-			id: '124',
-			name: 'Sally',
-			email: 'sally@gmail.com',
-			password: 'bananas',
-			entries: 0,
-			joined: new Date()
-		}
-	]
-}
+// const database = {
+// 	users: [
+// 		{
+// 			id: '123',
+// 			name: 'John',
+// 			email: 'john@gmail.com',
+// 			password: 'cookies',
+// 			entries: 0,
+// 			joined: new Date()
+// 		},
+// 		{
+// 			id: '124',
+// 			name: 'Sally',
+// 			email: 'sally@gmail.com',
+// 			password: 'bananas',
+// 			entries: 0,
+// 			joined: new Date()
+// 		}
+// 	]
+// }
 
 app.get('/', (req, res) => {
-	res.send(database.users);
+	res.send('success');
 })
 
 app.post('/signin', (req, res) => {
@@ -112,7 +112,7 @@ app.put('/image', (req, res) => {
 		.increment('entries', 1)
 		.returning('entries')
 		.then(entries => {
-			console.log(entries[0].entries);
+			res.json(entries[0].entries);
 		})
 		.catch(err => res.status(400).json('Unable to get entries'))
 })
