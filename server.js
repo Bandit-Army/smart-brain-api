9a2fbba2b880c04d04c2f8be03567ec48208a4ea
Joe Lee <joe.p.lee@outlook.com>
2024-09-26 09:55:36 -0500

    Added database env variables

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ const image = require('./controllers/image')
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
-    port: 5432,
-    user: 'postgres',
-    password: 'postgres',
-    database: 'smart-brain',
+    host: process.env.DATABASE_URL,
+    PGHOST= process.env.PGHOST
+		PGDATABASE= process.env.PGDATABASE
+		PGUSER= process.env.PGUSER
+		PGPASSWORD= process.env.PGPASSWORD
   },
 });
 
